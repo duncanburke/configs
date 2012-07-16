50894c76501d068c05b4ef01c3767880a4438d26
Duncan Burke <duncan.burke@orionvm.com.au>
2012-07-17 06:21:17 +1000

    add install.sh
    Signed-off-by: Duncan Burke <duncan.burke@orionvm.com.au>

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+manifest="
+bashrc:$HOME/.bashrc
+emacs:$HOME/.emacs
+gitconfig:$HOME/.gitconfig
+xinitrc:$HOME/.xinitrc
+xmobarrc:$HOME/.xmobarrc
+xmonad.hs:$HOME/.xmonad/xmonad.hs
+xresources:$HOME/.Xresources
+site-lisp:/usr/share/emacs/site-lisp
+"
+for file in $manifest; do
+src="${DIR}/${file%%:*}"
+dest=${file##*:}
+path=${dest%/*}
+mkdir -p $path
+ln -s $src $dest
+done
