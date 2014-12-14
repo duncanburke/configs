d2b5fa91d8e5f5ceaf1b6a1e6e412c3edd859189
Duncan Burke <duncankburke@gmail.com>
2014-12-14 14:10:39 +1100

    xmonad: rewrite, proper dvorak bindings

--- xmonad.hs
+++ xmonad.hs
@@ -1,5 +1,8 @@
 import Control.Applicative
 import System.IO (hPutStrLn)
+import System.Exit
+
+import qualified Data.Map as Map
 
 import XMonad
 import XMonad.Hooks.DynamicLog (xmobarPP, ppOutput, ppTitle,
@@ -8,42 +11,68 @@ import XMonad.Hooks.DynamicLog (xmobarPP, ppOutput, ppTitle,
                                 xmobarColor)
 import XMonad.Hooks.ManageDocks (manageDocks, avoidStruts)
 import XMonad.Util.Run (spawnPipe)
-import XMonad.Util.EZConfig (additionalKeys)
 import XMonad.StackSet (greedyView, shift, view)
+import qualified XMonad.StackSet as W
 
 main :: IO ()
 main = do
      xmproc <- spawnPipe "/home/duncan/.cabal/bin/xmobar /home/duncan/.xmobarrc"
-     let conf = defaultConfig {
-           workspaces = (show <$> ([1..9] ++ [0] :: [Int])) ++ (take nFn fnWorkspaceIds),
-           manageHook = manageDocks <+>
-                        composeAll [] <+>
-                        manageHook defaultConfig,
+     xmonad $ defaultConfig {
+           workspaces = workspaceIds,
            layoutHook = avoidStruts $ layoutHook defaultConfig,
+           terminal = "urxvtc",
            normalBorderColor = "#000000",
            focusedBorderColor = "#1793d1",
-           terminal = "urxvtc",
+           modMask = mod4Mask,
+           keys = \conf -> Map.fromList $ keys' conf,
            logHook = dynamicLogWithPP $ xmobarPP {
              ppOutput = hPutStrLn xmproc,
              ppTitle = xmobarColor "green" "" . shorten 190},
-           modMask = mod4Mask }
-     xmonad $ (conf `additionalKeys` keyBindings)
+           manageHook = manageHook'}
 
 type Keybinding = ((KeyMask, KeySym), X ())
 
-keyBindings :: [Keybinding]
-keyBindings = commandBindings ++
-              workspaceBindings ++
-              screenBindings
+manageHook' :: ManageHook
+manageHook' = manageDocks <+>
+              composeAll []
 
-workspaceBindings :: [Keybinding]
-workspaceBindings = workspaceMap xK_0 "0" ++
-                    ((take nFn $ zip [xK_F1..] fnWorkspaceIds) >>= (uncurry workspaceMap))
+keys' :: XConfig Layout -> [Keybinding]
+keys' conf = commandBindings conf ++
+             workspaceBindings ++
+             screenBindings
 
-commandBindings :: [Keybinding]
-commandBindings = [((mod4Mask, xK_p), spawn "dmenu_run"),
-                   ((mod4Mask, xK_z), spawn "mpc toggle"),
-                   ((mod4Mask, xK_x), spawn "mpc next")]
+commandBindings :: XConfig Layout -> [Keybinding]
+commandBindings conf@(XConfig {XMonad.modMask = modMask'}) =
+  [((modMask' .|. shiftMask, xK_Return ), spawn $ terminal conf),
+   ((modMask'              , xK_p      ), spawn "dmenu_run"),
+   ((modMask'              , xK_z      ), spawn "mpc toggle"),
+   ((modMask'              , xK_x      ), spawn "mpc next"),
+   ((modMask' .|. shiftMask, xK_j      ), kill),
+   ((modMask'              , xK_space  ), sendMessage NextLayout),
+   ((modMask' .|. shiftMask, xK_space  ), setLayout $ layoutHook conf),
+   ((modMask' .|. shiftMask, xK_q      ), io (exitWith ExitSuccess)),
+   ((modMask'              , xK_q      ), spawn "if type xmonad; then xmonad --recompile && xmonad --restart; else xmessage xmonad not in \\$PATH: \"$PATH\"; fi"),
+
+   ((modMask'              , xK_n      ), refresh),
+
+   ((modMask'              , xK_Tab    ), windows W.focusDown),
+   ((modMask' .|. shiftMask, xK_Tab    ), windows W.focusUp),
+   ((modMask'              , xK_c      ), windows W.focusDown),
+   ((modMask'              , xK_r      ), windows W.focusUp),
+   ((modMask'              , xK_s      ), windows W.focusMaster),
+
+   ((modMask'              , xK_Return ), windows W.swapMaster),
+   ((modMask' .|. shiftMask, xK_c      ), windows W.swapDown),
+   ((modMask' .|. shiftMask, xK_r      ), windows W.swapUp),
+
+   ((modMask'              , xK_g      ), sendMessage Shrink),
+   ((modMask'              , xK_l      ), sendMessage Expand),
+
+   ((modMask'              , xK_d      ), withFocused $ windows . W.sink),
+
+   ((modMask'              , xK_b      ), sendMessage (IncMasterN 1)),
+   ((modMask'              , xK_m      ), sendMessage (IncMasterN (-1)))]
+
 
 screenBindings :: [Keybinding]
 screenBindings = [((m .|. mod4Mask, key), screenWorkspace sc >>= flip whenJust (windows . f)) |
@@ -60,6 +89,15 @@ workspaceMap :: KeySym -> WorkspaceId -> [Keybinding]
 workspaceMap k w = [((mod4Mask, k), windows $ greedyView w),
                     ((shiftMask .|. mod4Mask, k),
                      windows $ shift w)]
+
+workspaceIds :: [WorkspaceId]
+workspaceIds = (show <$> ([1..9] ++ [0] :: [Int])) ++ (take nFn fnWorkspaceIds)
+
+workspaceKeys :: [KeySym]
+workspaceKeys = [xK_1..xK_9] ++ [xK_0] ++ (take nFn [xK_F1..])
+
+workspaceBindings :: [Keybinding]
+workspaceBindings = zip workspaceKeys workspaceIds >>= (uncurry workspaceMap)
 
 screenKeys :: [KeySym]
 screenKeys = [xK_h, xK_t, xK_n]
