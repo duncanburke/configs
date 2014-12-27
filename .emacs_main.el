3e6a016019d39c1cea5aa2ab1a5dc40d7bd43e22
Duncan Burke <duncankburke@gmail.com>
2014-12-27 23:10:11 +1100

    emacs: add org-mode package archive

--- .emacs_main.el
+++ .emacs_main.el
@@ -8,6 +8,8 @@
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives
+             '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
 
 (defun ensure-package-installed (&rest packages)
