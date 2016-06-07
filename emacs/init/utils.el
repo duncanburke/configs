f0c39bcb8a3ddb5b3997fb2aee4c7b1c5fc23559
Duncan Burke <duncankburke@gmail.com>
2016-06-07 11:34:02 +1000

    emacs: add fill-or-unfill to utils

--- emacs/init/utils.el
+++ emacs/init/utils.el
@@ -109,4 +109,14 @@
   (interactive)
   (switch-to-buffer "*scratch*"))
 
+(defun fill-or-unfill ()
+  "Like `fill-paragraph', but unfill if used twice."
+  (interactive)
+  (let ((fill-column
+         (if (eq last-command 'endless/fill-or-unfill)
+             (progn (setq this-command nil)
+                    (point-max))
+           fill-column)))
+    (call-interactively #'fill-paragraph)))
+
 (provide 'utils)
