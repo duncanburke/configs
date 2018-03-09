6aced0bffb6063bfe1cdd2a28b7a17ed5b17cfa7
Duncan Burke <duncankburke@gmail.com>
2018-03-09 17:15:51 +1100

    emacs: enable narrow-to-* commands

--- emacs/init/config.el
+++ emacs/init/config.el
@@ -58,6 +58,8 @@
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
 (put 'set-goal-column 'disabled nil)
+(put 'narrow-to-region 'disabled nil)
+(put 'narrow-to-page 'disabled nil)
 
 ;; System-specific
 
